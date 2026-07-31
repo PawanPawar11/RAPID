e364a55f8f745eadff69561ec71ff8b64ab9751f
PawanPawar11 <pawanpawar4369@gmail.com>
2026-07-31 17:28:57 +0530

    feat: implement basic TCP server that accepts a single client

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+TcpListener listener = new TcpListener(IPAddress.Any, 6379);
+
+listener.Start();
+
+TcpClient client = listener.AcceptTcpClient();
+
+NetworkStream stream = client.GetStream();
+
+byte[] buffer = new byte[1024];
+
+int bytesRead = stream.Read(buffer, 0, buffer.Length);
+
+string message = Encoding.UTF8.GetString(buffer, 0, bytesRead);
+
+Console.WriteLine(message);
+
+string response = "OK";
+
+byte[] responseBytes = Encoding.UTF8.GetBytes(response);
+
+stream.Write(responseBytes);
+
+stream.Close();
+client.Close();
+listener.Stop();
